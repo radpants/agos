59061ae6bc1232d7531b0719034315daceb360d3
AJ Austinson <aj.austinson@gmail.com>
2020-12-26 15:17:35 -0800

    Added raylib as submodule

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <raylib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    InitWindow(1280, 720, "Agos");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Agos!", 32, 32, 16, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
